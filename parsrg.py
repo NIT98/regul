da89513af8cb158b4c8a1ea95fe48539af1991d6
SajjadSeifi <s.09334497255@gmail.com>
2022-02-27 15:37:20 -0800

    build : base of dfa regular expersion

---
+++ parsrg.py
@@ -0,0 +1,25 @@
+from lexrg import LexerRegul
+
+def parse(lex : LexerRegul):
+    return regex(lex)
+
+def regex(lex : LexerRegul):
+    pass
+
+def expr(lex : LexerRegul):
+    pass
+
+def sizing(lex : LexerRegul):
+    pass
+
+def item(lex : LexerRegul):
+    pass
+
+def range(lex : LexerRegul):
+    pass
+
+def digit(lex : LexerRegul):
+    pass
+
+def ch(lex : LexerRegul):
+    pass
