3211c1f0164a26371a4a0e3923468cc83a470d4b
SajjadSeifi <s.09334497255@gmail.com>
2022-02-27 15:02:12 -0800

    build : next character getter

--- lexrg.py
+++ lexrg.py
@@ -3,3 +3,13 @@ class LexerRegul:
         self.input = input
         self.pos = 0
 
+    def nextc(self) -> str:
+        if len(self.input) <= self.pos:
+            return ''
+
+        c = self.input[self.pos] 
+        self.pos += 1
+
+        return c
+
+        
