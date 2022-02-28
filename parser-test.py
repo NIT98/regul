e6085f3a24b3677a5fd39b504db9e0b787f07509
SajjadSeifi <s.09334497255@gmail.com>
2022-02-27 17:04:34 -0800

    test : add 3 test case for test parse function

--- parser-test.py
+++ parser-test.py
@@ -1,6 +1,8 @@
-from parso import parse
-from lexrg import LexerRegul
-lex = LexerRegul("/^[0-9]+$/")
+from traceback import print_list
+from parsrg import parse
+from lexrg import LexerRegul as Lexer
 
-print("#Test Parse Nethod :")
-parse(lex)
+print("#Test Parse Method :")
+parse(Lexer("/^[0-9]+$/"))
+parse(Lexer("0/^[0-9]+$/"))
+parse(Lexer("/^[0-9]+$/0"))
