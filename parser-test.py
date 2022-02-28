b2cc87e0fbca3e76ae6541b7e927f2350bd20bcf
SajjadSeifi <s.09334497255@gmail.com>
2022-02-28 13:35:22 -0800

    add : tree logger for test

--- parser-test.py
+++ parser-test.py
@@ -1,6 +1,8 @@
+from loging import logtree
 from parsrg import parse
 from lexrg import LexerRegul as Lexer
 
 print("#Test Parse Method :")
 root = parse(Lexer("/^([^a-z]+|P){0,1}$/"))
-print(root)
+
+logtree(root.toany3())
