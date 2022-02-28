48bc2e4d5f1f7b9ddaf292458a3fa8cbdb0a1985
SajjadSeifi <s.09334497255@gmail.com>
2022-02-28 14:02:47 -0800

    refct : use updated utils convertor

--- parser-test.py
+++ parser-test.py
@@ -1,8 +1,8 @@
 from loging import logtree
 from parsrg import parse
 from lexrg import LexerRegul as Lexer
+from utils import toany3d
 
 print("#Test Parse Method :")
 root = parse(Lexer("/^([^a-z]+|P){0,1}$/"))
-
-logtree(root.toany3())
+logtree(toany3d(root))
