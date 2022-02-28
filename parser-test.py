f771cc7312b1645d87fc6778a342256ba2a09682
SajjadSeifi <s.09334497255@gmail.com>
2022-02-28 07:39:51 -0800

    add bad test case

--- parser-test.py
+++ parser-test.py
@@ -3,7 +3,7 @@ from parsrg import parse
 from lexrg import LexerRegul as Lexer
 
 print("#Test Parse Method :")
-parse(Lexer("/^[a-z]{0,1}$/"))
+parse(Lexer("/^([^a-z]+|P){0,1$/"))
 # parse(Lexer("/^[0-9]+$/"))
 # parse(Lexer("0/^[0-9]+$/"))
 # parse(Lexer("/^[0-9]+$/0"))
