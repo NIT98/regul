f34cd49f436e4e26a8c8e70a70fe322422a0e6ad
SajjadSeifi <s.09334497255@gmail.com>
2022-02-28 12:42:48 -0800

    fixed : add '}' & print ast root

--- parser-test.py
+++ parser-test.py
@@ -1,7 +1,6 @@
-from traceback import print_list
 from parsrg import parse
 from lexrg import LexerRegul as Lexer
 
 print("#Test Parse Method :")
-root = parse(Lexer("/^([^a-z]+|P){0,1$/"))
+root = parse(Lexer("/^([^a-z]+|P){0,1}$/"))
 print(root)
