46a302ce78ae5021e0e6e68bb01db316c54c08a1
SajjadSeifi <s.09334497255@gmail.com>
2022-02-28 17:26:55 -0800

    add input & position accepted

--- regul.py
+++ regul.py
@@ -4,11 +4,14 @@ from lexrg import LexerRegul
 from parsrg import parse
 
 class Regul:
+    
     def __init__(self,ast : ASTNode) -> None:
         self.ast = ast
+        self.input = None
+        self.cpos = -1
 
     def compile(pattern:str) -> Self:
         lex = LexerRegul(pattern)
         prs = parse(lex)
         return Regul(prs)
-    
+
